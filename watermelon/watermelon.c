70875d97999ca6dcec72f5ea8b525bbf4d7c07ef
Safoun <safoun.official@gmail.com>
2025-09-29 15:15:40 +0600

    repo for problem solving files

---
+++ watermelon/watermelon.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, result;
+    scanf("%d", &num);
+    if (num == 2 || num % 2 != 0)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+    return 0;
+}
