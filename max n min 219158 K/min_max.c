d1627e3e8aa2dde598c7b96de995b14d0225742c
Safoun <safoun.official@gmail.com>
2025-10-04 22:15:53 +0600

    min max solved with a hichki

---
+++ max n min 219158 K/min_max.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, min, max;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c)
+    {
+        max = a;
+    }
+    else if (b > a && b > c)
+    {
+        max = b;
+    }
+    else if (a == b && a > c)
+    {
+        max = a;
+    }
+    else if ( b == c && b > a)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+
+    if (a < b && a < c)
+    {
+        min = a;
+    }
+    else if (b < a && b < c)
+    {
+        min = b;
+    }
+    else if (a == b && a < c)
+    {
+        min = a;
+    }
+    else if (b == c && b < a)
+    {
+        min = b;
+    }
+    else
+    {
+        min = c;
+    }
+
+    printf("%d %d", min, max);
+    return 0;
+}
